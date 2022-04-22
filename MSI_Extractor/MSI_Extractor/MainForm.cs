b0bf63dc94fedb13614775ec778a2f09280a0f78
Tiurabe103 <104230918+Tiurabe103@users.noreply.github.com>
2022-04-22 18:31:02 -0300

    Add files via upload

---
+++ MSI_Extractor/MSI_Extractor/MainForm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace MSI_Extractor
+{
+	public partial class MainForm : Form
+	{
+		private string MSIFilePath;
+		private string TargetFolder;
+		
+		public MainForm()
+		{
+			InitializeComponent();
+			tbFolderPath.ReadOnly = true;
+			tbFilePath.ReadOnly = true;
+			
+		}
+		
+		void BtnSelectFileClick(object sender, EventArgs e)
+		{
+			OpenFileDialog openFileDialog = new OpenFileDialog();
+			openFileDialog.Filter = "Microsoft Installer|*.msi";
+
+			if (openFileDialog.ShowDialog() == DialogResult.OK) {
+  				MSIFilePath = openFileDialog.FileName;
+  				tbFilePath.Text = MSIFilePath;
+			}
+		}
+		
+		void BtnFolderSelectClick(object sender, EventArgs e)
+		{
+			FolderBrowserDialog openFolderDialog = new FolderBrowserDialog();
+
+			if (openFolderDialog.ShowDialog() == DialogResult.OK) {
+				TargetFolder = openFolderDialog.SelectedPath;
+  				tbFolderPath.Text = TargetFolder;
+			}
+		}
+		
+		void BtnExtractClick(object sender, EventArgs e)
+		{
+			if (string.IsNullOrEmpty(MSIFilePath) || string.IsNullOrEmpty(TargetFolder)) {
+				MessageBox.Show("Ambos os campos não podem estar vazios.", "Erro");
+  				return;
+			}
+			
+			Process process = new Process();
+			process.StartInfo.FileName = "msiexec.exe";
+			process.StartInfo.Arguments = "/a"+ '"' + MSIFilePath + '"' +"TARGETDIR="+ '"' +TargetFolder + '"';
+			process.Start();
+			
+		}
+		
+	}
+}
